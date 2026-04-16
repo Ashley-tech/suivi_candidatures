ffde0b763f7bb2d9eef24f05b582cce8b779d58f
Ashley R. <ashleyrakotoarisoa@gmail.com>
2026-04-16 21:55:27 +0200

    Add routes offres, candidatures, cv, compte

--- candidatures_web/routes/web.php
+++ candidatures_web/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Compte;
+use App\Models\Candidature;
+use App\Models\Offre;
+use App\Models\CV;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +22,43 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/compte', function () {
-    
+Route::get('/comptes', function () {
+    return Compte::all();
+});
+
+Route::post('/comptes', function (Request $request) {
+    return Compte::create($request->all());
+});
+
+Route::post('/login', function (Request $request) {
+    $compte = Compte::where('email', $request->email)->first();
+    if ($compte && password_verify($request->mdp, $compte->mdp_crypted)) {
+        return response()->json(['message' => 'Login successful', 'compte' => $compte], 200);
+    } else {
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+});
+
+Route::get("/comptes/{id}", function ($id) {
+    return Compte::find($id);
+});
+
+Route::get("/candidatures", function () {
+    return Candidature::all();
+});
+
+Route::get("/{compte}/candidatures", function ($compte) {
+    return Candidature::where('compte', $compte)->get();
+});
+
+Route::get("/{compte}/cvs", function ($compte) {
+    return CV::where('compte', $compte)->get();
+});
+
+Route::get("/offres", function () {
+    return Offre::all();
+});
+
+Route::get("/offres/{id}", function ($id) {
+    return Offre::find($id);
 });
