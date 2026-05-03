3b675b38a5ce0e6143ca48537e48da379295ab50
Ashley R. <ashleyrakotoarisoa@gmail.com>
2026-05-03 20:21:35 +0200

    Préparation Redis

---
+++ candidatures_web/app/Http/Controllers/RedisController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RedisController extends Controller
+{
+    public function setKey($key,$value,$ttl){
+        $redis = app()->make('redis');
+        $redis->set($key, $value);
+        $redis->expire($key, $ttl);
+    }
+
+    public function getAllKeys(){
+        $redis = app()->make('redis');
+        return $redis->keys('*');
+    }
+
+    public function getValue($key){
+        $redis = app()->make('redis');
+        return $redis->get($key);
+    }
+
+    public function deleteKey($key){
+        $redis = app()->make('redis');
+        $redis->del($key);
+    }
+}
