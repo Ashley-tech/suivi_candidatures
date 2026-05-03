8e6237f8fe3c1d9d87a291b291330b33934de952
Ashley R. <ashleyrakotoarisoa@gmail.com>
2026-05-03 17:06:57 +0200

    Protected offre

--- candidatures_web/app/Models/Offre.php
+++ candidatures_web/app/Models/Offre.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Offre extends Model
 {
     use HasFactory;
+    protected $table = 'offre';
 
     protected $fillable = ['titre','type','description','nom_entreprise','adresse_entreprise','adresse_comp_entreprise','cp_entreprise','ville_entreprise','pays_entreprise','nom_recruteur','prenom_recruteur','email_entreprise','tel_entreprise','periode','salaire_min','salaire_max','date_publication'];
 
