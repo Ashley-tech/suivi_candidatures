eee41ef1cab8f3067f09332844a18e8a043ce3fc
Ashley R. <ashleyrakotoarisoa@gmail.com>
2026-05-04 14:53:09 +0200

    Finalisation page bienvenue

--- candidatures_web/resources/views/welcome.blade.php
+++ candidatures_web/resources/views/welcome.blade.php
@@ -4,24 +4,28 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Candidatures - Accueil</title>
+        <title>Candidatures - Bienvenue</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        
     </head>
     <body>
         <h1>Bienvenue sur le suivi de candidatures</h1>
-        <p>Ce projet a pour but de suivre les candidatures que vous avez faites, et de vous aider à organiser votre recherche d'emploi.</p>
-        <button id="continuer">Continuer</button>
+        <p style="font-size: 30px">Cette application a pour but de suivre les candidatures que vous avez faites, et de vous aider à organiser votre recherche d'emploi.<br />
+        Vous pourrez aussi matcher vos candidatures avec les offres d'emploi que vous aurez notées.<br />
+        Vous pouvez aussi mettre à jour votre profil et vos coordonnées, ainsi que le statut de vos candidatures à tout moment.<br /><br />Bon courage pour votre recherche !</p>
+        <button id="continuer" style="position: fixed; bottom: 20px; right: 20px;">Continuer</button>
     </body>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.5/dist/js.cookie.min.js"></script>
     <script>
         $("#continuer" ).on( "click", function( event ) {
-            if (!Cookies.get("login")) {
+            //if (!Cookies.get("login")) {
+            if (!sessionStorage.getItem("login")) {
                 location.href = "/login";
             }else{
                 location.href = "/dashboard";
