884c3068451c1b44d6f42998eb3d9f1b8c6f6c5e
Ashley R. <ashleyrakotoarisoa@gmail.com>
2026-05-07 21:34:28 +0200

    Finalisation interface bienvenue

--- candidatures_android/app/src/main/java/com/example/suivicandidatures/WelcomeActivity.kt
+++ candidatures_android/app/src/main/java/com/example/suivicandidatures/WelcomeActivity.kt
@@ -2,10 +2,17 @@ package com.example.suivicandidatures
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
     }
+
+    fun suite(view: View) {
+        
+    }
+
+
 }
