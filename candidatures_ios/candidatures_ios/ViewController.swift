a1bce7849b9f4df4de4ef690f866ce3591fffaa1
Ashley-tech <ashleyrakotoarisoa@gmail.com>
2026-05-18 15:49:44 +0200

    Correction iOS

---
+++ candidatures_ios/candidatures_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  candidatures_ios
+//
+//  Created by Ashley Rakotoarisoa on 18/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ candidatures_ios/candidatures_iosTests/candidatures_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  candidatures_iosTests.swift
+//  candidatures_iosTests
+//
+//  Created by Ashley Rakotoarisoa on 18/05/2026.
+//
+
+import Testing
+@testable import candidatures_ios
+
+struct candidatures_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
