873f1be7df7009781fc8699eeaeb57f4b9c8a5d8
Ashley-tech <ashleyrakotoarisoa@gmail.com>
2026-05-21 22:03:08 +0200

    Interfaces Menu

--- candidatures_ios/candidatures_ios/MenuViewController.swift
+++ candidatures_ios/candidatures_ios/MenuViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class MenuViewController: UIViewController {
 
+    @IBOutlet weak var welcomet: UILabel!
+    @IBOutlet weak var messager: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
---
+++ candidatures_ios/candidatures_ios/SignupResponse.swift
@@ -0,0 +1,15 @@
+//
+//  SignupResponse.swift
+//  candidatures_ios
+//
+//  Created by Ashley Rakotoarisoa on 21/05/2026.
+//
+
+import Foundation
+
+struct SignupResponse {
+    let message: String,
+         compte: Compte,
+        success: Bool,
+        code: Int
+}
