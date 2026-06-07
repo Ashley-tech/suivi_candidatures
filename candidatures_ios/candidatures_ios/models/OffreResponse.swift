b137376466ffe55fc1a80962ed7f102e50c95470
Ashley-tech <ashleyrakotoarisoa@gmail.com>
2026-06-07 22:11:34 +0200

    Ajout Offre/OffreResponse

--- candidatures_ios/candidatures_ios/models/OffreResponse.swift
+++ candidatures_ios/candidatures_ios/models/OffreResponse.swift
@@ -8,22 +8,7 @@
 import Foundation
 
 struct OffreResponse : Decodable {
-    let id: Int
-    let type: String?
-    let titre : String
-    let description: String?
-    let nom_entreprise: String?
-    let adresse_entreprise: String?
-    let adresse_comp_entreprise: String?
-    let cp_entreprise: String?
-    let ville_entreprise: String?
-    let pays_entreprise: String?
-    let nom_recruteur : String?
-    let prenom_recruteur: String?
-    let email_entreprise: String?
-    let tel_entreprise: String?
-    let periode: String?
-    let salaire_min: Double?
-    let salaire_max: Double?
-    let date_publication: String?
+    let message: String
+        let success: Bool
+        let offre_id: Int?
 }
